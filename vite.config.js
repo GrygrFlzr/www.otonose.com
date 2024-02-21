6ec869287c81503b0af5ee2bfcde018657bcb3a8
GrygrFlzr <grygrflzr@hotmail.com>
2024-02-22 02:49:04 +0900

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		minify: 'terser',
+		// oldest version with >=0.5% usage in JP
+		target: ['chrome109', 'safari15.8', 'firefox121']
+	}
 });
